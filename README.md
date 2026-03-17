6c2b64e319b84aa795f6d63406f50efe7a182b71
Bruno <brunoalexandre59@gmail.com>
2026-03-17 15:46:40 +0000

    adiciona script `lint:check` e `link:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
-Aprendendo programação 
+
+Aprendendo programação
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home(){
-    return <h1>Olá Mundo!</h1>
+function Home() {
+  return <h1>Olá Mundo!</h1>;
 }
 
-export default Home
+export default Home;
